a74b8717ab7989ba92bdfe26e462cf516eb1c91c
axelherreram <aherreram22@miumg.edu.gt>
2024-08-01 19:59:47 -0600

    modelo para el time line de los usuarios

---
+++ models/timelineEventos.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+const Usuario = require("./usuario");
+
+const TimelineEventos = sequelize.define("TimelineEventos", {
+  evento_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Usuario,
+      key: "user_id",
+    },
+  },
+  tipoEvento: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  descripcion: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  fecha:{
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+
+},{
+    timestamps: false,
+    tableName: 'TimelineEventos'
+}
+);
+
+module.exports = TimelineEventos;
