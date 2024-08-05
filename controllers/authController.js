1614904eb8c16687f063809f8aa9c231aa0e8359
axelherreram <aherreram22@miumg.edu.gt>
2024-08-05 00:57:41 -0600

    configuracion de swagger

---
+++ controllers/authController.js
@@ -0,0 +1,108 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Usuarios = require("../models/usuarios");
+
+const registerUser = async (req, res) => {
+  const { email, password, nombre, carnet, sede_id, rol_id, anioRegistro } = req.body;
+
+  try {
+    let user = await Usuarios.findOne({ where: { email } });
+
+    if (user) {
+      return res.status(400).json({ message: "Usuario ya registrado" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = await Usuarios.create({
+      email,
+      password: hashedPassword,
+      nombre,
+      carnet,
+      sede_id,
+      rol_id,
+      anioRegistro
+    });
+    res.status(201).json({ message: "Usuario registrado exitosamente" });
+  } catch (err) {
+    res.status(500).json({ message: "Error en el servidor" });
+  }
+};
+
+
+/* 
+Pendiente agregar sede para iniciar sesion
+  Esto para que solo los usuairos de la sede puedan iniciar sesion
+*/
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await Usuarios.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    const token = jwt.sign(
+      { user: { user_id: user.user_id, rol_id: user.rol_id } },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    res.status(200).json({
+      message: 'Login successful',
+      email: user.email,
+      userName: user.nombre,
+      carnet: user.carnet,
+      sede: user.sede_id,
+      rol: user.rol_id,
+      anio: user.anioRegistro,
+      token
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+  
+
+
+const updateUser = async (req, res) => {
+  const { password } = req.body;
+  const user_id = req.user ? req.user.user_id : null; 
+
+  if (!password) {
+    return res.status(400).json({ message: 'Nueva contraseña requerida' });
+  }
+
+  if (!user_id) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    await Usuarios.update({ password: hashedPassword }, { where: { user_id } });
+
+    res.json({ message: 'Contraseña actualizada exitosamente' });
+  } catch (err) {
+    res.status(500).json({ message: 'Error en el servidor' });
+  }
+};
+
+  
+
+module.exports = {
+  registerUser,
+  loginUser,
+  updateUser,
+};
---
+++ docs/swagger.js
@@ -0,0 +1,35 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'User API',
+      version: '1.0.0',
+      description: 'API for user management',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  apis: ['./routes/*.js'], 
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+module.exports = {
+  swaggerUi,
+  swaggerDocs,
+};
