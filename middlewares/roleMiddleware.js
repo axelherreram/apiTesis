099b05d34db08d67d8732dacd3d29da3f48e6f5c
axelherreram <aherreram22@miumg.edu.gt>
2024-08-05 14:47:44 -0600

    middleware para verificar roles

---
+++ middlewares/roleMiddleware.js
@@ -0,0 +1,30 @@
+// middlewares/roleMiddleware.js
+const Usuarios = require('../models/usuarios');
+
+const verifyRole = (rolesPermitidos) => {
+  return async (req, res, next) => {
+    const userId = req.user ? req.user.user_id : null;
+
+    if (!userId) {
+      return res.status(401).json({ message: 'No autorizado' });
+    }
+
+    try {
+      const user = await Usuarios.findByPk(userId);
+
+      if (!user) {
+        return res.status(404).json({ message: 'Usuario no encontrado' });
+      }
+
+      if (!rolesPermitidos.includes(user.rol_id)) {
+        return res.status(403).json({ message: 'Acceso denegado' });
+      }
+
+      next();
+    } catch (error) {
+      res.status(500).json({ message: 'Error en el servidor' });
+    }
+  };
+};
+
+module.exports = verifyRole;
