0f29cbc5f48b2a6420b057a40a7805ef9a9d0244
axelherreram <aherreram22@miumg.edu.gt>
2024-08-04 23:50:16 -0600

    Se borro modelo de ciclos

--- config/iniciableTables.js
+++ config/iniciableTables.js
@@ -1,6 +1,5 @@
 const Entregas = require("../models/entregas");
 const BitacoraApp = require("../models/bitacoraApp");
-const Ciclos = require("../models/ciclos");
 const Cursos = require("../models/cursos");
 const notas = require("../models/notas");
 const PropuestaTesis = require("../models/propuestaTesis");
@@ -21,26 +20,7 @@ const initializetables = async () => {
     // Creacion de sedes
     await Sedes.findOrCreate({ where: { nombreSede: "Guastatoya" } });
     await Sedes.findOrCreate({ where: { nombreSede: "Sanarate" } });
-    
-    // creacion de los ciclos
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "1-2018" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "2-2018" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "1-2019" } });
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "2-2019" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "1-2020" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "2-2020" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "1-2021" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "2-2021" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "1-2022" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "2-2022" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "1-2023" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "2-2023" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "1-2024" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "2-2024" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "1-2025" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "2-2025" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "1-2026" } });  
-    await Ciclos.findOrCreate({ where: { nombreCiclo: "2-2026" } });  
+
 
     await Cursos.findOrCreate({ where: { nombreCurso: "Proyecto de Graduación I" } });
     await Cursos.findOrCreate({ where: { nombreCurso: "Proyecto de Graduación II" } });
