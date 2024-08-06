70c0168b85dbdee8c8c3e06c2230877574752431
axelherreram <aherreram22@miumg.edu.gt>
2024-08-05 21:09:44 -0600

    CRUD de propuestas de tesis

---
+++ controllers/propuestaTesisController.js
@@ -0,0 +1,91 @@
+const PropuestaTesis = require("../models/propuestaTesis");
+
+// Listar propuestas de tesis de un usuario
+const listarPropuestasPorUsuario = async (req, res) => {
+  const { user_id } = req.params;
+  try {
+    const propuestas = await PropuestaTesis.findAll({ where: { user_id } });
+    res.status(200).json(propuestas);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error al obtener las propuestas de tesis", error });
+  }
+};
+
+// Crear una nueva propuesta de tesis
+const crearPropuesta = async (req, res) => {
+  const { user_id, titulo, propuesta } = req.body;
+
+  try {
+    const count = await PropuestaTesis.count({ where: { user_id } });
+    if (count >= 3) {
+      return res.status(400).json({
+        message:
+          "No se pueden tener más de 3 propuestas. Elimine o actualice una existente.",
+      });
+    }
+
+    await PropuestaTesis.create({
+      user_id,
+      titulo,
+      propuesta,
+    });
+    res.status(201).json({
+      message: "Propuesta creada correctamente",
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error al crear la propuesta de tesis", error });
+  }
+};
+
+// Actualizar una propuesta de tesis
+const actualizarPropuesta = async (req, res) => {
+  const { propuesta_id } = req.params;
+  const { titulo, propuesta } = req.body;
+
+  try {
+    const propuestaExistente = await PropuestaTesis.findByPk(propuesta_id);
+    if (!propuestaExistente) {
+      return res.status(404).json({ message: "Propuesta no encontrada" });
+    }
+    propuestaExistente.titulo = titulo;
+    propuestaExistente.propuesta = propuesta;
+
+    await propuestaExistente.save();
+
+    res.status(200).json({
+      message: "Propuesta actualizada correctamente",
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error al actualizar la propuesta", error });
+  }
+};
+
+// Eliminar una propuesta de tesis
+const eliminarPropuesta = async (req, res) => {
+  const { propuesta_id } = req.params;
+
+  try {
+    const propuesta = await PropuestaTesis.findByPk(propuesta_id);
+    if (!propuesta) {
+      return res.status(404).json({ message: "Propuesta no encontrada" });
+    }
+
+    await propuesta.destroy();
+    res.status(200).json({ message: "Propuesta eliminada correctamente" });
+  } catch (error) {
+    res.status(500).json({ message: "Error al eliminar la propuesta", error });
+  }
+};
+
+module.exports = {
+  listarPropuestasPorUsuario,
+  crearPropuesta,
+  actualizarPropuesta,
+  eliminarPropuesta,
+};
