8023b179f72591ee0c22f24f7734d8a1dc97270e
axelherreram <aherreram22@miumg.edu.gt>
2024-08-07 12:42:08 -0600

    Se agrego el registro para la bicacora

--- controllers/sedeController.js
+++ controllers/sedeController.js
@@ -1,4 +1,5 @@
 const Sede = require('../models/sede');
+const { registrarBitacora } = require('../sql/bitacora');
 
 const listarSedes = async (req, res) => {
   try {
@@ -9,21 +10,26 @@ const listarSedes = async (req, res) => {
   }
 };
 
-// Crear una nueva sede
 const crearSede = async (req, res) => {
-    try {
-      const { nombreSede } = req.body;
-      if (!nombreSede) {
-        return res.status(400).json({ message: 'El nombre de la sede es necesario' });
-      }
-      const nuevaSede = await Sede.create({ nombreSede });
-      res.status(201).json({
-        message: 'Sede creada satisfactoriamente',
-      });
-    } catch (error) {
-      res.status(500).json({ message: 'Error al crear la sede', error: error });
+  try {
+    const { nombreSede } = req.body;
+    const user_id = req.user_id; 
+
+    if (!nombreSede) {
+      return res.status(400).json({ message: 'El nombre de la sede es necesario' });
     }
-  };
+        
+    // Aquí agregarías el registro a la bitácora
+    registrarBitacora(user_id, 'Creación de sede', `Una nueva sede: (${nombreSede}), fue creada.`);
+    await Sede.create({ nombreSede });
+
+
+    res.status(201).json({ message: 'Sede creada satisfactoriamente' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al crear la sede', error });
+  }
+};
+
   
 
 module.exports = { listarSedes, crearSede };
