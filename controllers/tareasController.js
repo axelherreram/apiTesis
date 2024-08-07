4048457bc5e65023c4cc0981dec07444b46709d9
axelherreram <aherreram22@miumg.edu.gt>
2024-08-07 13:04:46 -0600

    Se agrego el registro para la bicacora

--- controllers/tareasController.js
+++ controllers/tareasController.js
@@ -1,8 +1,17 @@
 const Tareas = require("../models/tareas");
+const { registrarBitacora } = require("../sql/bitacora");
 
 const listarTareas = async (req, res) => {
+  const user_id = req.user_id; 
   try {
     const tareas = await Tareas.findAll();
+
+    // Aquí agregamos el registro a la bitácora antes de enviar la respuesta
+    await registrarBitacora(
+      user_id,
+      "Obtner todas las tareas",
+      `Listo todas las tareas`
+    );
 
     res.status(200).json(tareas);
   } catch (error) {
@@ -13,6 +22,7 @@ const listarTareas = async (req, res) => {
 const actualizarTarea = async (req, res) => {
   const { tarea_id } = req.params;
   const { titulo, descripcion, inicioTarea, finTarea } = req.body;
+  const user_id = req.user_id; 
   try {
     const tarea = await Tareas.findByPk(tarea_id);
     if (!tarea) {
@@ -23,9 +33,17 @@ const actualizarTarea = async (req, res) => {
     tarea.descripcion = descripcion ?? tarea.descripcion;
     tarea.inicioTarea = inicioTarea ?? tarea.inicioTarea;
     tarea.finTarea = finTarea ?? tarea.finTarea;
+    
+    // Aquí agregamos el registro a la bitácora después de guardar la tarea
+    await registrarBitacora(
+      user_id,
+      `Actualizar tarea con id: ${tarea_id}`,
+      `Cambios: Titulo ${tarea.titulo} -> ${titulo}, Descripcion: ${tarea.descripcion} -> ${descripcion}, InicioTarea: ${tarea.inicioTarea} -> ${inicioTarea}, FinTarea: ${tarea.finTarea} -> ${finTarea} fue actualizada`
+    );
 
     await tarea.save();
-    res.status(200).json({ message: "Tarea actualizada exitosamente"});
+
+    res.status(200).json({ message: "Tarea actualizada exitosamente" });
   } catch (error) {
     res.status(500).json({ message: "Error al actualizar la tarea", error });
   }
