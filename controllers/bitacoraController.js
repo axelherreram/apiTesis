d343e3ba11670121c17ec49483dd5ccda3ea8910
axelherreram <aherreram22@miumg.edu.gt>
2024-08-10 16:00:11 -0600

    Nuevo controlador para obtener todas la bitacoras

--- controllers/bitacoraController.js
+++ controllers/bitacoraController.js
@@ -1,4 +1,33 @@
 const BitacoraApp = require('../models/bitacoraApp'); 
+const Usuarios = require('../models/usuarios');
+
+
+const listarTodasBitacoras = async (req, res) => {
+  try {
+    const bitacoras = await BitacoraApp.findAll();
+
+    if (!bitacoras || bitacoras.length === 0) {
+      return res.status(404).json({ message: 'No se encontraron entradas de bitácora' });
+    }
+
+    const formattedBitacoras = bitacoras.map((bitacora) => ({
+      usuario: bitacora.usuario,
+      accion: bitacora.accion,
+      descripcion: bitacora.detalles,
+      fecha: bitacora.fecha,
+    }));
+
+    res.json({
+      message: 'Lista completa de bitácoras',
+      bitacoras: formattedBitacoras,
+    });
+  } catch (err) {
+    console.error('Error al listar todas las bitácoras:', err);
+    res.status(500).json({ message: 'Error en el servidor' });
+  }
+};
+
+
 
 const listarBitacoraPorUsuario = async (req, res) => {
   const user_id = req.params.user_id;
@@ -9,12 +38,21 @@ const listarBitacoraPorUsuario = async (req, res) => {
     if (!bitacoras || bitacoras.length === 0) {
       return res.status(404).json({ message: 'No se encontraron entradas de bitácora para este usuario' });
     }
+    const formattedBitacoras = bitacoras.map((bitacora) => ({
+      username: bitacora.username,
+      accion: bitacora.accion,
+      descripcion: bitacora.descripcion,
+      fecha: bitacora.fecha,
+    }));
 
-    res.json(bitacoras);
+    res.json({
+      message: 'Lista de bitácoras',
+      bitacoras: formattedBitacoras,
+    });
   } catch (err) {
     console.error('Error al listar las bitácoras:', err);
     res.status(500).json({ message: 'Error en el servidor' });
   }
 };
 
-module.exports = { listarBitacoraPorUsuario };
+module.exports = { listarBitacoraPorUsuario, listarTodasBitacoras };
