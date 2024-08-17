06cb6f1226a8f29aeb89882bd930e48cf1d5c64f
axelherreram <aherreram22@miumg.edu.gt>
2024-08-17 14:31:19 -0600

    get all toles

--- controllers/rolController.js
+++ controllers/rolController.js
@@ -3,13 +3,7 @@ const Role = require("../models/roles");
 
 const listarRoles = async (req, res) => {
   try {
-    const roles = await Role.findAll({
-      where: {
-        rol_id: {
-          [Op.in]: [1, 2],
-        },
-      },
-    });
+    const roles = await Role.findAll();
     res.status(200).json(roles);
   } catch (error) {
     res.status(500).json({ message: "Error al obtener los roles", error });
