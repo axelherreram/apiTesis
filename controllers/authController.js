5b0a50d20ffc744e2e365dae7fc5ad4c068119e9
axelherreram <aherreram22@miumg.edu.gt>
2024-08-19 19:43:42 -0600

    Nueva ruta para actualizar foto de perfil

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,48 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const Usuarios = require("../models/usuarios");
-const { registrarBitacora } = require('../sql/bitacora');
+const { registrarBitacora } = require("../sql/bitacora");
+const multer = require('multer');
 const CursoAsignacion = require("../models/cursoAsignacion");
+const path = require('path'); // Asegúrate de importar path aquí
+const fs = require('fs');
+
+// Configuración de multer para limitar los tipos de archivos
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '../public/fotoPerfil'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+// Filtro de archivos para aceptar solo PNG y JPG
+const fileFilter = (req, file, cb) => {
+  const allowedMimeTypes = ['image/jpeg', 'image/png'];
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    cb(null, true); // Acepta el archivo
+  } else {
+    cb(new Error('Solo se permiten archivos PNG y JPG'), false); // Rechaza el archivo
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+});
 
 const registerUser = async (req, res) => {
-  const { email, password, nombre, carnet, sede_id, rol_id, anioRegistro, curso_id } = req.body;
+  const {
+    email,
+    password,
+    nombre,
+    carnet,
+    sede_id,
+    rol_id,
+    anioRegistro,
+    curso_id,
+  } = req.body;
 
   try {
     let user = await Usuarios.findOne({ where: { email } });
@@ -24,9 +61,10 @@ const registerUser = async (req, res) => {
       });
 
       if (!curso_id) {
-        return res.status(201).json({ message: "Usuario registrado exitosamente" });
+        return res
+          .status(201)
+          .json({ message: "Usuario registrado exitosamente" });
       }
-
     }
 
     // Verificar si el usuario ya está asignado a este curso
@@ -38,7 +76,11 @@ const registerUser = async (req, res) => {
     });
 
     if (asignacionExistente) {
-      return res.status(400).json({ message: `El usuario ya está asignado al curso con ID ${curso_id}` });
+      return res
+        .status(400)
+        .json({
+          message: `El usuario ya está asignado al curso con ID ${curso_id}`,
+        });
     }
 
     // Asignar el curso al usuario
@@ -47,93 +89,161 @@ const registerUser = async (req, res) => {
       curso_id: curso_id,
     });
 
-    res.status(201).json({ message: "Usuario registrado y curso asignado exitosamente" });
+    res
+      .status(201)
+      .json({ message: "Usuario registrado y curso asignado exitosamente" });
   } catch (err) {
-    res.status(500).json({ message: "Error en el servidor", error: err.message });
+    res
+      .status(500)
+      .json({ message: "Error en el servidor", error: err.message });
   }
 };
 
-
-
 const loginUser = async (req, res) => {
-  const { email, password, sede_id } = req.body;
+  const { email, password } = req.body;
 
   try {
-    const user = await Usuarios.findOne({ where: { email, sede_id } });
+    const user = await Usuarios.findOne({ where: { email } });
 
     if (!user) {
-      const userExists = await Usuarios.findOne({ where: { email } });
-      if (!userExists) {
-        return res.status(404).json({ message: 'Usuario no encontrado' });
-      }
-      return res.status(403).json({ message: 'No pertenece a esta sede' });
-    }  
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid password' });
+      return res.status(401).json({ message: "Contraseña inválida" });
     }
 
     const token = jwt.sign(
-      { user: { user_id: user.user_id, rol_id: user.rol_id, sede_id: user.sede_id } },
+      {
+        user: {
+          user_id: user.user_id,
+          rol_id: user.rol_id,
+          sede_id: user.sede_id,
+        },
+      },
       process.env.JWT_SECRET,
-      { expiresIn: '1h' }
+      { expiresIn: "1h" }
     );
-    // Scrip para registrar en la bitacora
-    registrarBitacora(user.user_id, user.nombre,`El usuario inició sesión`, 'Inicio de sesión');
+
+    // Registrar en la bitácora
+    registrarBitacora(
+      user.user_id,
+      user.nombre,
+      "El usuario inició sesión",
+      "Inicio de sesión"
+    );
 
     res.status(200).json({
-      message: 'Login successful',
+      message: "Inicio de sesión exitoso",
       email: user.email,
       userName: user.nombre,
       carnet: user.carnet,
       sede: user.sede_id,
       rol: user.rol_id,
       anio: user.anioRegistro,
-      token
+      token,
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-  
-
-
-const updateUser = async (req, res) => {
+const actualizarPassword = async (req, res) => {
   const { password } = req.body;
-  const user_id = req.user ? req.user.user_id : null; 
-
-  if (!password) {
-    return res.status(400).json({ message: 'Nueva contraseña requerida' });
-  }
+  const user_id = req.user ? req.user.user_id : null;
 
   if (!user_id) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return res.status(401).json({ message: "Unauthorized" });
   }
-  
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ message: "Se requiere una nueva contraseña" });
+  }
 
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await Usuarios.findOne({where: user_id})
-    
     await Usuarios.update({ password: hashedPassword }, { where: { user_id } });
-    
-    // Scrip para registrar en la bitacora
-    await registrarBitacora(user_id, user.nombre, `El usuario actualizo su contraseña`, 'Actualización de contraseña');
 
-    res.json({ message: 'Contraseña actualizada exitosamente' });
+    const userUpdated = await Usuarios.findOne({ where: { user_id } });
+
+    // Registrar la actualización en la bitácora
+    await registrarBitacora(
+      user_id,
+      userUpdated.nombre,
+      `El usuario actualizó su contraseña`,
+      "Actualización de contraseña"
+    );
+
+    res.json({ message: "Contraseña actualizada exitosamente" });
   } catch (err) {
-    res.status(500).json({ message: 'Error en el servidor' });
+    res
+      .status(500)
+      .json({ message: "Error en el servidor", error: err.message });
   }
 };
 
-  
+const actualizarFotoPerfil = async (req, res) => {
+  const user_id = req.user ? req.user.user_id : null;
+  const fotoPerfil = req.file ? req.file.filename : null;
 
+  if (!user_id) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  if (!fotoPerfil) {
+    return res
+      .status(400)
+      .json({ message: "No se proporcionó una nueva foto de perfil" });
+  }
+
+  try {
+    const user = await Usuarios.findOne({ where: { user_id } });
+
+    if (!user) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+
+    if (user.FotoPerfil) {
+      const oldImagePath = path.join(__dirname, '../public/fotoPerfil', user.FotoPerfil);
+
+      if (fs.existsSync(oldImagePath)) {
+        fs.unlink(oldImagePath, (err) => {
+          if (err) {
+            console.log(`Error al eliminar la foto anterior: ${err.message}`);
+          } else {
+            console.log('Foto de perfil anterior eliminada correctamente');
+          }
+        });
+      } else {
+        console.log('La foto de perfil anterior no existe');
+      }
+    }
+
+    await Usuarios.update({ FotoPerfil: fotoPerfil }, { where: { user_id } });
+
+    const userUpdated = await Usuarios.findOne({ where: { user_id } });
+
+    await registrarBitacora(
+      user_id,
+      userUpdated.nombre,
+      `El usuario actualizó su foto de perfil`,
+      "Actualización de foto de perfil"
+    );
+
+    res.json({ message: "Foto de perfil actualizada exitosamente" });
+  } catch (err) {
+    res.status(500).json({ message: "Error en el servidor", error: err.message });
+  }
+};
 module.exports = {
   registerUser,
   loginUser,
-  updateUser,
+  actualizarPassword,
+  actualizarFotoPerfil,
+  upload
 };
