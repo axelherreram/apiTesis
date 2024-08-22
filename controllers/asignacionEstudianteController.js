3b124776cbaf79df6cdf313b9e1d64f1a3e340c3
axelherreram <aherreram22@miumg.edu.gt>
2024-08-21 23:09:28 -0600

    Se añadio registro para la bitacora

--- controllers/asignacionEstudianteController.js
+++ controllers/asignacionEstudianteController.js
@@ -1,8 +1,10 @@
 const AsignacionEstudiante = require("../models/asignacionEstudiante");
 const { registrarBitacora } = require("../sql/bitacora");
 const Usuario = require("../models/usuarios");
+const Sede = require("../models/sede");
 
 const crearAsignacion = async (req, res) => {
+  const user_id = req.user_id;
   try {
     const { catedratico_id, estudiante_id } = req.body;
 
@@ -17,12 +19,21 @@ const crearAsignacion = async (req, res) => {
       estudiante_id,
     });
 
-    return res
-      .status(201)
-      .json({
-        message: "Asignación creada exitosamente",
-        data: nuevaAsignacion,
-      });
+    const User = await Usuario.findByPk(user_id);
+    const Catedratico = await Usuario.findByPk(catedratico_id);
+    const Estudiante = await Usuario.findByPk(estudiante_id);
+
+    await registrarBitacora(
+      user_id,
+      User.nombre,
+      `Creó una asignación para el estudiante: ${Estudiante.nombre} con el catedrático: ${Catedratico.nombre}`,
+      'Creacion de asignación para estudiante' 
+    );
+
+    return res.status(201).json({
+      message: "Asignación creada exitosamente",
+      data: nuevaAsignacion,
+    });
   } catch (error) {
     return res
       .status(500)
@@ -42,12 +53,19 @@ const listarEstudiantesPorCatedratico = async (req, res) => {
       where: {
         catedratico_id,
       },
-      attributes: ['asignacion_id'],
+      attributes: ["catedratico_id"],
       include: [
         {
           model: Usuario,
-          as: "estudiante", 
-          attributes: ["user_id", "nombre", "email", "carnet", "fotoPerfil", "sede_id"], 
+          as: "estudiante",
+          attributes: ["user_id", "nombre", "email", "carnet", "fotoPerfil"],
+          // include: [
+          //   {
+          //     model: Sede,
+          //     as: "sede",
+          //     attributes: ["nombreSede"],
+          //   },
+          // ],
         },
       ],
     });
@@ -57,6 +75,23 @@ const listarEstudiantesPorCatedratico = async (req, res) => {
         message: `No se encontraron estudiantes asignados para el catedrático con ID ${catedratico_id}`,
       });
     }
+
+    const Catedratico = await Usuario.findByPk(catedratico_id, {
+      include: [
+        {
+          model: Sede,
+          as: "sede",
+          attributes: ["nombreSede"],
+        },
+      ],
+    });
+
+    await registrarBitacora(
+      catedratico_id,
+      Catedratico.nombre,
+      `Listó los estudiantes asignados a su cuenta con sede: ${Catedratico.sede.nombreSede}`,
+      "Listar estudiantes"
+    );
 
     return res.status(200).json({
       message: "Estudiantes asignados",
@@ -71,8 +106,7 @@ const listarEstudiantesPorCatedratico = async (req, res) => {
   }
 };
 
-
 module.exports = {
-    crearAsignacion,
-    listarEstudiantesPorCatedratico
+  crearAsignacion,
+  listarEstudiantesPorCatedratico,
 };
