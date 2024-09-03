471dce832a9933177d36ad5888e5cf76d1d02359
axelherreram <aherreram22@miumg.edu.gt>
2024-09-02 21:30:08 -0600

    Modelo de tipos de tareas

---
+++ models/typeTask.js
@@ -0,0 +1,22 @@
+const { DataTypes, Model } = require("sequelize");
+const sequelize = require("../config/database");
+
+const typeTask = sequelize.define("typeTask", {
+    typeTask_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true, 
+  }
+},{
+  timestamps: false,
+  tableName: 'typeTask'
+});
+
+
+
+module.exports = typeTask;
