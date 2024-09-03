f774a34f40ff1a253e42d8cc2bde4bde205d708e
axelherreram <aherreram22@miumg.edu.gt>
2024-09-02 21:33:12 -0600

    Crear tarea

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,7 +1,60 @@
 const Task = require("../models/task");
 const { logActivity } = require("../sql/appLog");
 const User = require("../models/user");
-const { where } = require("sequelize");
+
+const createTask = async (req, res) => {
+  const {
+    course_id,
+    sede_id,
+    typeTask_id,
+    title,
+    description,
+    taskStart,
+    endTask,
+  } = req.body;
+  const user_id = req.user_id;
+
+  try {
+    const tareaExistente  = await Task.findOne({
+      where: { course_id, sede_id, typeTask_id: 1 },
+    });
+
+    if (tareaExistente ) {
+      return res.status(400).json({
+        message: "Tarea de propuesta de tesis ya existe!",
+      });
+    }
+
+    const newTask = await Task.create({
+      course_id,
+      sede_id,
+      typeTask_id,
+      title,
+      description,
+      taskStart,
+      endTask,
+    });
+    // Registrar la actividad en la bitácora
+    const user = await User.findByPk(user_id);
+    await logActivity(
+      user_id,
+      user.sede_id,
+      user.name,
+      `El usuario creó una nueva tarea con título: ${title}`,
+      "Creación de tarea"
+    );
+
+    res.status(201).json({
+      message: "Tarea creada exitosamente",
+      data: newTask,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al crear la tarea",
+      error: error.message || error,
+    });
+  }
+};
 
 const listTasks = async (req, res) => {
   const { sede_id } = req.params;
@@ -88,6 +141,7 @@ const updateTask = async (req, res) => {
 };
 
 module.exports = {
+  createTask,
   listTasks,
   updateTask,
   listTasksByCourse,
