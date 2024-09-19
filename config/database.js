a93758a66db5c9274c511bae2fb0f078091eae1f
axelherreram <aherreram22@miumg.edu.gt>
2024-09-18 19:12:11 -0600

    Ajuste a la conexion

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
-  dialect: 'mysql',
+  dialect: 'mysql',  // o el dialecto que estés usando
   logging: false
 });
 
-module.exports = sequelize;
+module.exports = { sequelize, Sequelize  };  // Exportamos la instancia de sequelize
