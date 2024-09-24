e38ce9e3ba3cb101c0b25b402d9b70035531c090
axelherreram <aherreram22@miumg.edu.gt>
2024-09-23 20:13:35 -0600

    Modificación de la tabla de timeline y asociaciones a los modelos

--- models/asociaciones.js
+++ models/asociaciones.js
@@ -8,47 +8,56 @@ const groupTerna = require('./groupTerna');
 const rolTerna = require('./rolTerna');
 const ternaAsignGroup = require('./ternaAsignGroup');
 const Roles = require('./roles');
+const TimelineEventos = require('./timelineEventos');
+const Task = require('./task');
 
 module.exports = function associateModels() {
-  // Relación entre User y Sede (muchos usuarios pertenecen a una sede)
+  // Relación entre User y Sede
   User.belongsTo(Sede, { foreignKey: 'sede_id', as: 'location' });
   Sede.hasMany(User, { foreignKey: 'sede_id' });
 
-  // Relación entre User y CourseAssignment (un estudiante puede tener muchas asignaciones de curso)
+  // Relación entre User y CourseAssignment
   User.hasMany(CourseAssignment, { foreignKey: 'student_id' });
   CourseAssignment.belongsTo(User, { foreignKey: 'student_id' });
 
   // Relación entre CourseAssignment y Course
   CourseAssignment.belongsTo(Course, { foreignKey: 'course_id' });
 
-  // Relación entre User y StudentAssignment (un estudiante o un instructor puede estar asignado a muchas actividades)
+  // Relación entre User y StudentAssignment
   User.hasMany(StudentAssignment, { foreignKey: 'student_id', as: 'studentAssignments' });
   User.hasMany(StudentAssignment, { foreignKey: 'instructor_id', as: 'instructorAssignments' });
 
-  // Relación entre StudentAssignment y User (como estudiante y como instructor)
+  // Relación entre StudentAssignment y User
   StudentAssignment.belongsTo(User, { foreignKey: 'student_id', as: 'student' });
   StudentAssignment.belongsTo(User, { foreignKey: 'instructor_id', as: 'instructor' });
 
-  // Relación entre CourseSedeAssignment, Course y Sede (muchos cursos pueden estar asignados a muchas sedes)
+  // Relación entre CourseSedeAssignment, Course y Sede
   CourseSedeAssignment.belongsTo(Course, { foreignKey: 'course_id' });
   CourseSedeAssignment.belongsTo(Sede, { foreignKey: 'sede_id' });
   Course.hasMany(CourseSedeAssignment, { foreignKey: 'course_id' });
   Sede.hasMany(CourseSedeAssignment, { foreignKey: 'sede_id' });
 
-  // Agregamos las nuevas relaciones para Terna
+  // Relación entre TimelineEventos, Task, User y Course
+  TimelineEventos.belongsTo(User, { foreignKey: 'user_id' });
+  TimelineEventos.belongsTo(Course, { foreignKey: 'course_id' });
+  TimelineEventos.belongsTo(Task, { foreignKey: 'task_id' });
+  Course.hasMany(TimelineEventos, { foreignKey: 'course_id' });
+  Task.hasMany(TimelineEventos, { foreignKey: 'task_id' });
+  User.hasMany(TimelineEventos, { foreignKey: 'user_id' });
 
-  // Relación entre ternaAsignGroup y User (una asignación de terna pertenece a un usuario)
+  // Relación entre Task y CourseSedeAssignment
+  Task.belongsTo(CourseSedeAssignment, { foreignKey: 'course_id' });
+
+  // Agregar relaciones de Terna
   ternaAsignGroup.belongsTo(User, { foreignKey: 'user_id' });
   User.hasMany(ternaAsignGroup, { foreignKey: 'user_id' });
 
-  // Relación entre ternaAsignGroup y groupTerna (una asignación de terna pertenece a un grupo)
   ternaAsignGroup.belongsTo(groupTerna, { foreignKey: 'groupTerna_id' });
   groupTerna.hasMany(ternaAsignGroup, { foreignKey: 'groupTerna_id' });
 
-  // Relación entre ternaAsignGroup y rolTerna (una asignación de terna tiene un rol)
   ternaAsignGroup.belongsTo(rolTerna, { foreignKey: 'rolTerna_id' });
   rolTerna.hasMany(ternaAsignGroup, { foreignKey: 'rolTerna_id' });
 
-
+  // Relación entre User y Roles
   User.belongsTo(Roles, { foreignKey: 'rol_id', as: 'role' });
 };
--- models/timelineEventos.js
+++ models/timelineEventos.js
@@ -1,6 +1,9 @@
 const { DataTypes } = require("sequelize");
 const User = require("./user");
 const { sequelize } = require('../config/database'); 
+const Course = require("./course");
+const Task = require("./task");
+
 
 const TimelineEventos = sequelize.define("TimelineEventos", {
   evento_id: {
@@ -23,6 +26,22 @@ const TimelineEventos = sequelize.define("TimelineEventos", {
   descripcion: {
     type: DataTypes.STRING(255),
     allowNull: false,
+  },
+  course_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Course,
+      key: "course_id",
+    },
+  },
+  task_id: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: Task,
+      key: "task_id",
+    },
   },
   fecha:{
     type: DataTypes.DATE,
