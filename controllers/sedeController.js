1b6781f4044a4c95b2866b31bd6d64293c9f878b
axelherreram <aherreram22@miumg.edu.gt>
2024-11-22 12:52:18 -0600

    Validación de sede unica

--- controllers/sedeController.js
+++ controllers/sedeController.js
@@ -24,16 +24,26 @@ const createSede = async (req, res) => {
         .json({ message: "El nombre de la sede es necesario" });
     }
 
+    // Verificar si la sede ya existe
+    const existingSede = await Sede.findOne({ where: { nameSede } });
+    if (existingSede) {
+      return res
+        .status(409)
+        .json({ message: `La sede '${nameSede}' ya existe` });
+    }
+
     const user = await User.findByPk(user_id);
 
-    // Script para registrar en la bitacora
+    // Script para registrar en la bitácora
     await logActivity(
       user_id,
       user.sede_id,
-      user.name, 
+      user.name,
       "Creación de sede",
       `Una nueva sede: (${nameSede}), fue creada.`
     );
+
+    // Crear la nueva sede
     await Sede.create({ nameSede });
 
     res.status(201).json({ message: "Sede creada satisfactoriamente" });
@@ -41,5 +51,6 @@ const createSede = async (req, res) => {
     res.status(500).json({ message: "Error al crear la sede", error });
   }
 };
+
 
 module.exports = { listSede, createSede };
