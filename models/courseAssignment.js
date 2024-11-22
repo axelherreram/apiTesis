dcf5ab0b5364038db4426f8f1bca6d4cee4faa35
axelherreram <aherreram22@miumg.edu.gt>
2024-11-22 12:57:21 -0600

    Cambio de campo incorrecto

--- models/courseAssignment.js
+++ models/courseAssignment.js
@@ -1,47 +1,36 @@
 const { DataTypes } = require("sequelize");
 const User = require("./user");
-const Course = require("./course");
-const { sequelize } = require("../config/database");  
-const Year = require("./year");
-const CourseAssignment = sequelize.define(
-  "CourseAssignment",  
-  {
-    courseAssignment_id: {  
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    student_id: {  // estudiante_id
-      type: DataTypes.INTEGER,
-      references: {
-        model: User,
-        key: "user_id",
-      },
-    },
-    course_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: Course,
-        key: "course_id",
-      },
-    },
-    year_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: Year,
-        key: "year_id",
-      },
+const CourseSedeAssignment = require("./courseSedeAssignment");
+const { sequelize } = require("../config/database");
+
+const CourseAssignment = sequelize.define("CourseAssignment", {
+  courseAssignment_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  student_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: "user_id",
     },
   },
-  {
-    timestamps: false,
-    tableName: "CourseAssignment", 
-  }
-);
+  asigCourse_id: {  // Relación con CourseSedeAssignment
+    type: DataTypes.INTEGER,
+    references: {
+      model: CourseSedeAssignment,
+      key: "asigCourse_id",
+    },
+  },
+}, {
+  timestamps: false,
+  tableName: "CourseAssignment",
+});
 
 CourseAssignment.associate = function(models) {
-  CourseAssignment.belongsTo(models.User, { foreignKey: 'student_id' });  // Usuarios -> User
-  CourseAssignment.belongsTo(models.Course, { foreignKey: 'course_id' });  // Cursos -> Course
+  CourseAssignment.belongsTo(models.User, { foreignKey: 'student_id' });  // Relación con User
+  CourseAssignment.belongsTo(models.CourseSedeAssignment, { foreignKey: 'asigCourse_id' });  // Relación con CourseSedeAssignment
 };
 
 module.exports = CourseAssignment;
