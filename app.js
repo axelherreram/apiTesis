4feee55c802e68c8924240e91b72af3cde4de452
axelherreram <aherreram22@miumg.edu.gt>
2024-12-23 12:54:42 -0600

    ajuste cors

--- app.js
+++ app.js
@@ -40,8 +40,10 @@ associateModels();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'http://127.0.0.1:5500'
+  'http://127.0.0.1:5500',
+  'http://localhost:3000' // Swagger URL
 ];
+
 
 app.use(cors({
   origin: function (origin, callback) {
