64aba0724759b6704f186db47b00ed7c87468fb0
axelherreram <aherreram22@miumg.edu.gt>
2025-02-18 10:04:34 +0600

    Eliminación de imports innecesarios

--- models/comments.js
+++ models/comments.js
@@ -1,20 +1,20 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/database");
-
-
+const User = require("./user");
+const Task = require("./task");
 /**
  * Model `Comments` represents a comment associated with a specific task and user.
- * 
+ *
  * Fields:
  * - `comment_id`: Unique identifier for the comment (PK, auto-increment).
  * - `user_id`: ID of the user who posted the comment (required).
  * - `task_id`: ID of the task to which the comment belongs (required).
  * - `comment_active`: Boolean indicating whether the comment is active or not (default: true).
- * 
+ *
  * Configuration:
  * - `timestamps: false`: Disables automatic `createdAt` and `updatedAt` fields.
  * - `tableName: "comments"`: Database table name (`comments`).
- * 
+ *
  * Relationships:
  * - `belongsTo(User, { foreignKey: "user_id" })`: A comment belongs to a user.
  * - `belongsTo(Task, { foreignKey: "task_id" })`: A comment belongs to a task.
@@ -29,18 +29,26 @@ const Comments = sequelize.define(
       autoIncrement: true,
     },
     user_id: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       allowNull: false,
+      references: {
+        model: User,
+        key: "user_id",
+      },
     },
     task_id: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       allowNull: false,
+      references: {
+        model: Task,
+        key: "task_id",
+      },
     },
     comment_active: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
       defaultValue: true,
-    }
+    },
   },
   {
     tableName: "comments",
