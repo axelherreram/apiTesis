4b7c7e56dc24a4243812c097ecfc0264094a8fd0
axelherreram <aherreram22@miumg.edu.gt>
2025-03-06 11:54:03 +0600

    ajuste de nombre de valor

--- config/iniciableTables.js
+++ config/iniciableTables.js
@@ -20,7 +20,7 @@ const initializeTables = async () => {
     await Roles.findOrCreate({ where: { name: "Estudiante" } });
     await Roles.findOrCreate({ where: { name: "Catedrático" } });
     await Roles.findOrCreate({ where: { name: "Administrador" } });
-    await Roles.findOrCreate({ where: { name: "SuperAdmin" } });
+    await Roles.findOrCreate({ where: { name: "Secretario/a" } });
     await Roles.findOrCreate({ where: { name: "Decano" } });
     await Roles.findOrCreate({ where: { name: "Cordinador de tesis" } });
     await Roles.findOrCreate({ where: { name: "Revisor" } });
